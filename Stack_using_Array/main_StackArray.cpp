f70447882bd763fdedb30c7e14c26e2cbde52b19
Hashir <hashir.logic@gmail.com>
2025-09-08 18:08:31 +0500

    Initial commit for DSA

---
+++ Stack_using_Array/main_StackArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+void decToBin(int num)
+
+int main(int argc, char** argv)
+{
+	int arr[5];
+	return 0;
+}
+void decToBin(int num)
+{
+	Stack<int> s1(arr.length());
+	int arr[5];
+	while(num!=0)
+	{
+		s1.push(num%2);
+		num=num/2;
+	}
+	while(!isEmpty())
+	{
+		cout<<s1.pop();
+	}
+}
